c6cef4f06e6e08062719519a4274d7b7261d90cf
haqhu <haqhuy1999@gmail.com>
2019-07-27 16:59:34 +0700

    inittal commit

---
+++ resources/js/components/users/User.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="User">
+        <p>hello word</p>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang='scss' scoped>
+    .User{
+        width: 100%;
+        background: #737373;
+        p{
+            color:red;
+        }
+    }
+</style>
